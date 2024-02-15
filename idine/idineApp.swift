580c2db6dcf053b413739816c5e61b6d7bdf7981
Igor Shcherba <igor.shcherba@callstack.com>
2024-02-15 15:38:03 +0100

    Initial Commit

---
+++ idine/idineApp.swift
@@ -0,0 +1,17 @@
+//
+//  idineApp.swift
+//  idine
+//
+//  Created by Igor Shcherba on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct idineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
