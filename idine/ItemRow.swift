c517822ff7f98b1f36ade8cc5095b632d6b13f6b
Igor Shcherba <igor.shcherba@callstack.com>
2024-02-25 11:23:29 +0100

    feat: add navigation link to ItemRow

--- idine/ItemRow.swift
+++ idine/ItemRow.swift
@@ -11,25 +11,25 @@ struct ItemRow: View {
     var item: MenuItem
     let colors: [String: Color] = ["V": .green, "D": .purple, "N": .red, "G": .black, "S": .blue]
     var body: some View {
-        HStack {
-            Image(item.thumbnailImage)
-                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                .overlay(Circle().stroke(.gray, lineWidth: 2))
+        NavigationLink {
+            Text(item.name)
+        } label: {
+            HStack {
+                Image(item.thumbnailImage)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(.gray, lineWidth: 2))
 
-            VStack(alignment: .leading) {
-                Text(item.name)
-                    .font(.headline)
-                Text("$\(item.price)")
-            }
+                VStack(alignment: .leading) {
+                    Text(item.name)
+                        .font(.headline)
+                    Text("$\(item.price)")
+                }
 
-            ForEach(item.restrictions, id: \.self) { restriction in
-                let color = colors[restriction, default: .black]
-                RestrictionBadge(restriction: restriction, bgColor: color)
+                ForEach(item.restrictions, id: \.self) { restriction in
+                    let color = colors[restriction, default: .black]
+                    RestrictionBadge(restriction: restriction, bgColor: color)
+                }
             }
         }
     }
-}
-
-#Preview {
-    ItemRow(item: MenuItem.example)
 }
