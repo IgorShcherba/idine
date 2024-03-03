33dfdb11fda3250e4b08eae1feeb279e21a6099d
Igor Shcherba <igor.shcherba@callstack.com>
2024-03-03 16:09:45 +0100

    feat: add OrderView

--- idine/Checkout/CheckoutView.swift
+++ idine/Checkout/CheckoutView.swift
@@ -11,23 +11,54 @@ struct CheckoutView: View {
     @EnvironmentObject var order: Order
     let paymentMethods: [String] = ["Cash", "Credit Card", "iDine Points"]
 
+    let tipAmounts: [Int] = [10, 15, 20, 25, 0]
+
+    var totalPrice: String {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+        return (total + tipValue).formatted(.currency(code: "USD"))
+    }
+
     @State private var paymentType = "Cash"
+    @State private var addLoayaltyDetails = false
+    @State private var loayaltyNumber = ""
+    @State private var tipAmount = 15
+    @State private var showingPaymentAlert = false
 
     var body: some View {
-        VStack {
+        Form {
             Section {
-                Text("Picked method is: \(paymentType)")
                 Picker("How do you want to pay", selection: $paymentType) {
                     ForEach(paymentMethods, id: \.self) { method in
                         Text(method)
                     }
                 }
+                Toggle("Add iDine loyalty card", isOn: $addLoayaltyDetails.animation())
+                if addLoayaltyDetails {
+                    TextField("Enter your iDine ID", text: $loayaltyNumber)
+                }
             }
-            .pickerStyle(.wheel)
-            .padding()
+
+            Section("Add a tip?") {
+                Picker("Percentage:", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) {
+                        Text("\($0)")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+
+            Section("Total: \(totalPrice)") {
+                Button("Confirm Order") {
+                    showingPaymentAlert.toggle()
+                }
+            }
         }
         .navigationTitle("Payment")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Order confirmed", isPresented: $showingPaymentAlert) {} message: {
+            Text("Your total was \(totalPrice), thank you!")
+        }
     }
 }
 
